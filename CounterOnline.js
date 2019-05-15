3106e6b064ce124a60346989782106f8453e576c
SuHouyu <553070703@qq.com>
2019-05-16 00:34:39 +0800

    Add files via upload

--- CounterOnline.js
+++ CounterOnline.js
@@ -1,1 +1,38 @@
+/**/
+var InValue;
 
+function GetValue(InValue) {
+  if (InValue == 0) {
+    alert(0);
+  } else if (InValue == 1) {
+    alert(0);
+  } else if (InValue == 2) {
+    alert(0);
+  } else if (InValue == 3) {
+    alert(0);
+  } else if (InValue == 4) {
+    alert(0);
+  } else if (InValue == 5) {
+    alert(0);
+  } else if (InValue == 6) {
+    alert(0);
+  } else if (InValue == 7) {
+    alert(0);
+  } else if (InValue == 8) {
+    alert(0);
+  } else if (InValue == 9) {
+    alert(0);
+  } else if (InValue == 1) {
+    alert(0);
+  } else if (InValue == 1) {
+    alert(0);
+  } else if (InValue == 1) {
+    alert(0);
+  } else if (InValue == 1) {
+    alert(0);
+  } else if (InValue == 1) {
+    alert(0);
+  } else if (InValue == 'equal') {
+    alert("Result is xxx");
+  }
+}
