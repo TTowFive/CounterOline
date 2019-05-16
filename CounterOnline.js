ab4318c30ba0d974bba796012ba81513543febf3
SuHouyu <553070703@qq.com>
2019-05-16 14:47:49 +0800

    Add files via upload

--- CounterOnline.js
+++ CounterOnline.js
@@ -1,61 +1,100 @@
 /**/
-var InValue;
-var Num = [];
+var InValue; //用来判断行为的值
+var Num = []; //存放用户输入数字
+var InsertNum; //用户输入时要插入Num的数字
+var Symbol_store; //符号储存
+var Numout; //用于数据显示
+var BeforeNum = 0; //被数
+var AlterNum = 0;
 
-// function COUNT() {
-//   var count = 0;
-//   for (){
-//
-//   }
-// }
-
-function checkNumLenth() {
+function checkNumLenth(InsertNum) { //检查用户输入数字个数
   var Text = document.getElementById('result').innerHTML;
   Text = parseInt(Text);
   Text = Text.toString();
   var TextLength = Text.length;
 
-  if (TextLength > 10) {
+  if (TextLength < 10) { //小于10个数字自动添加数字
+    Num.push(InsertNum);
+    Numout = Num.join('');
+    Numout = parseFloat(Numout);
+    document.getElementById('result').innerHTML = Numout;
+  }
+
+  if (TextLength > 10) { //如果大于10个字符自动缩小
     alert(TextLength);
     document.getElementById('result').style['font-size'] = 75 + 'px';
   }
+
 }
 
-function GetValue(InValue) {
+function Clear() {
+  document.getElementById('result').innerHTML = 0;
+  Num = [];
+  Numout = [];
+}
+
+function Calculate(SYM) {
+  var SUM = 0;
+  if (SYM == '+') {
+    SUM = BeforeNum + AlterNum;
+  } else if (SYM == '-') {
+    SUM = BeforeNum - AlterNum;
+  } else if (SYM == 'X') {
+    SUM = BeforeNum * AlterNum;
+  } else if (SYM == '/') {
+    SUM = BeforeNum / AlterNum
+  }
+  document.getElementById('result').innerHTML = SUM;
+  BeforeNum = SUM;
+}
+
+function GetValue(InValue) { //按键判断
   if (InValue == 0) {
     // document.getElementById('result').innerHTML = 0;
-    checkNumLenth();
-    // document.getElementById('result').style['font-size'] = '75px';
-
+    checkNumLenth(0);
   } else if (InValue == 1) {
-    alert(1);
+    checkNumLenth(1);
   } else if (InValue == 2) {
-    alert(2);
+    checkNumLenth(2);
   } else if (InValue == 3) {
-    alert(3);
+    checkNumLenth(3);
   } else if (InValue == 4) {
-    alert(4);
+    checkNumLenth(4);
   } else if (InValue == 5) {
-    alert(5);
+    checkNumLenth(5);
   } else if (InValue == 6) {
-    alert(6);
+    checkNumLenth(6);
   } else if (InValue == 7) {
-    alert(7);
+    checkNumLenth(7);
   } else if (InValue == 8) {
-    alert(8);
+    checkNumLenth(8);
   } else if (InValue == 9) {
-    alert(9);
-    // } else if (InValue == 1) {
-    //   alert(0);
-    // } else if (InValue == 1) {
-    //   alert(0);
-    // } else if (InValue == 1) {
-    //   alert(0);
-    // } else if (InValue == 1) {
-    //   alert(0);
-    // } else if (InValue == 1) {
-    //   alert(0);
+    checkNumLenth(9);
+  } else if (InValue == 'dot') {
+    checkNumLenth('.');
+  } else if (InValue == '+') {
+    BeforeNum = Numout;
+    Symbol_store = InValue;
+    Clear();
+  } else if (InValue == '-') {
+    BeforeNum = Numout;
+    Symbol_store = InValue;
+    Clear();
+  } else if (InValue == 'X') {
+    BeforeNum = Numout;
+    Symbol_store = InValue;
+    Clear();
+  } else if (InValue == '/') {
+    BeforeNum = Numout;
+    Symbol_store = InValue;
+    Clear();
+  } else if (InValue == 'AC') {
+    document.getElementById('result').innerHTML = 0;
+    Num = [];
+    Numout = [];
+    Symbol_store = '';
   } else if (InValue == 'equal') {
-    alert("Result is xxx");
+    AlterNum = Numout;
+    Calculate(Symbol_store);
   }
 }
